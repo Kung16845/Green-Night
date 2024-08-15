de1419d2737998ed7356ab2702d430a482c73f88
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2024-08-15 22:45:04 +0700

    ..

--- GreenNight/Assets/Build System/UIBuilding.cs
+++ GreenNight/Assets/Build System/UIBuilding.cs
@@ -9,6 +9,7 @@ public class UIBuilding : MonoBehaviour
    
     public Building building;
     public BuildManager buildManager;
+    public Image image;
     public TextMeshProUGUI textNameBuild;
     public TextMeshProUGUI textDescriveBuild;
 
@@ -16,7 +17,7 @@ public class UIBuilding : MonoBehaviour
     {
         textNameBuild.text = building.nameBuild;
         textDescriveBuild.text = building.detailBuild;
-        
+        image.sprite = building.GetComponent<Sprite>();
         buildManager.building = building;
     }
 }
