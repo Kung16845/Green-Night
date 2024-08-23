ad3b4f71d5f20df223ff994c09a278ab70641ede
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2024-08-24 01:35:10 +0700

    Create script class item no fuction

---
+++ GreenNight/Assets/Script/Item/ItemClass.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemClass : MonoBehaviour
+{   
+    [Header("Stat")]
+    public string nameItem;
+    public int quantityItem;
+    [Header("Item value")]
+    public int rarityItem;
+    public int tradeValueItem;
+    public bool carftableItem;
+    public int disassembleItem;
+    public int idItem;
+    [Header("DDA Stat")]
+    public string pointType;
+    public int point;
+
+    
+}
