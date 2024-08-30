073c6ffc2d0efe22651040dad04c45dfcc56fd04
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2024-08-31 01:38:21 +0700

    Inventory Add item

--- GreenNight/Assets/Script/Item/InventoryItem/InventoryItemPresent.cs
+++ GreenNight/Assets/Script/Item/InventoryItem/InventoryItemPresent.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using UnityEngine;
 
 public class InventoryItemPresent : MonoBehaviour
-{   
+{
     public List<ItemData> listItemsData = new List<ItemData>();
-    public List<ItemClass> listUIItem; 
+    public List<UIItemData> listUIItem;
     public Transform inventoryUI;
-    private void Start() {
+    private void Start()
+    {
         RefreshUI();
     }
     public void RefreshUI()
-    {   
+    {
         foreach (Transform child in inventoryUI)
         {
             Destroy(child.gameObject);
@@ -21,7 +22,58 @@ public class InventoryItemPresent : MonoBehaviour
         foreach (ItemData itemData in listItemsData)
         {
             GameObject uiItem = listUIItem.FirstOrDefault(idItem => idItem.idItem == itemData.idItem).gameObject;
-            Instantiate(uiItem,inventoryUI,false);
+            Instantiate(uiItem, inventoryUI, false);
+
+            UIItemData uIItemData = uiItem.GetComponent<UIItemData>();
+            uIItemData.UpdateDataUI(itemData);
+        }
+
+    }
+    public void AddItem(ItemData itemDataAdd)
+    {
+
+        ItemData itemDataInList = listItemsData.FirstOrDefault(item => item.idItem == itemDataAdd.idItem && item.count != item.maxCount);
+
+        if (itemDataInList != null)
+        {   
+            int itemCount = itemDataInList.count + itemDataAdd.count;
+            if (itemCount <= itemDataInList.maxCount)
+            {
+                itemDataInList.count += itemDataAdd.count;
+            }
+            else if (itemCount >= itemDataInList.maxCount)
+            {
+                Debug.Log("ITem new create count : " + itemDataAdd.count);
+
+                ItemData newItemData = new ItemData();
+                newItemData.nameItem = itemDataInList.nameItem;
+                newItemData.idItem = itemDataInList.idItem;
+                newItemData.count = itemCount - itemDataInList.maxCount;
+                newItemData.maxCount = itemDataInList.maxCount;
+                newItemData.itemtype = itemDataInList.itemtype;
+            
+                listItemsData.Add(newItemData);
+
+                itemDataInList.count = itemDataInList.maxCount;
+
+                
+            }
+        }
+        else
+        {
+            listItemsData.Add(itemDataAdd);
+        }
+
+        RefreshUI();
+    }
+    public void RemoveItem(ItemData itemDataRemove)
+    {
+
+        ItemData itemDataInList = listItemsData.FirstOrDefault(item => item.idItem == itemDataRemove.idItem);
+        itemDataInList.count--;
+        if (itemDataInList.count <= 0)
+        {
+            listItemsData.Remove(itemDataInList);
         }
 
     }
--- GreenNight/Assets/Script/Item/InventoryItem/itemData.cs
+++ GreenNight/Assets/Script/Item/InventoryItem/itemData.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 [Serializable]
 public class ItemData 
 {   
     public string nameItem;
     public int idItem;
+    public int count;
+    public int maxCount;
     public Itemtype itemtype;
 }
 public enum Itemtype
