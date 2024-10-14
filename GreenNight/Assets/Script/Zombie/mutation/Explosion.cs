a0bb0f51e5ee4872806c2d248ee64ceaab43f00d
Agentatnaf <111513753+AgentAtnaf@users.noreply.github.com>
2024-10-14 17:05:41 +0700

    Added Broken armour,nerf penetration, Add explosive mutation

--- GreenNight/Assets/Script/Zombie/mutation/Explosion.cs
+++ GreenNight/Assets/Script/Zombie/mutation/Explosion.cs
@@ -13,14 +13,32 @@ public class Explosion : MonoBehaviour
         this.barrierDamage = barrierDamage;
         this.zombieDamage = zombieDamage;
         this.radius = radius;
-
-        ApplyDamage();
-        Destroy(this.gameObject); // Destroy after applying damage
+        this.transform.localScale = new Vector3(radius * 2, radius * 2, 1f);
+        StartCoroutine(CreateArea());
+    }
+    private IEnumerator CreateArea()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(this.gameObject);
     }
 
     private void ApplyDamage()
     {
         // Apply damage to barriers and zombies within radius
         // (Implement collision detection and damage application here)
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Zombie zombie = other.GetComponent<Zombie>();
+        if (zombie != null)
+        {
+            zombie.ZombieTakeDamage(zombieDamage, DamageType.Explosive);
+        }
+
+        Barrier barrierComponent = other.GetComponent<Barrier>();
+        if (barrierComponent != null)
+        {
+            barrierComponent.BarrierTakeDamage(barrierDamage);
+        }
     }
 }
